15ca891362b150d1c027ef0b15af84de76137010
Marcos Rios <rioos.marcos@gmail.com>
2022-11-08 23:20:29 -0300

    Exercise 2.20: resolution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,14 +2,21 @@ import { useEffect, useState } from "react";
 import PersonRecord from "./Components/PersonRecord";
 import Search from "./Components/Search";
 import PhonebookForm from "./Components/PhonebookForm";
-import axios from "axios";
 import personsService from "./services/persons";
+import "./App.css";
 
-const baseUrl = "http://localhost:3001/persons";
+const uniqueId = (length = 16) =>
+  parseInt(
+    Math.ceil(Math.random() * Date.now())
+      .toPrecision(length)
+      .toString()
+      .replace(".", "")
+  );
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [searchResults, setSearchResults] = useState();
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     personsService.getAll().then((persons) => {
@@ -17,13 +24,20 @@ const App = () => {
     });
   }, []);
 
-  const uniqueId = (length = 16) =>
-    parseInt(
-      Math.ceil(Math.random() * Date.now())
-        .toPrecision(length)
-        .toString()
-        .replace(".", "")
-    );
+  const createMessage = (message, type) => {
+    setMessage({ message, type });
+    setTimeout(() => {
+      setMessage(undefined);
+    }, 5000);
+  };
+
+  const createSuccessMessage = (message) => {
+    createMessage(message, "success");
+  };
+
+  const createErrorMessage = (message) => {
+    createMessage(message, "error");
+  };
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
@@ -52,6 +66,7 @@ const App = () => {
                 person.id !== personExists.id ? person : updatedPerson
               )
             );
+            createSuccessMessage(`Updated ${newPerson.name}`);
           });
       }
       return;
@@ -61,6 +76,7 @@ const App = () => {
 
     personsService.create(newPerson).then((returnedPerson) => {
       setPersons(persons.concat(returnedPerson));
+      createSuccessMessage(`Added ${newPerson.name}`);
     });
   };
 
@@ -79,14 +95,26 @@ const App = () => {
   const deletePerson = (id) => () => {
     const person = persons.find((person) => person.id === id);
     if (window.confirm(`Delete ${person.name}?`)) {
-      personsService.deletePerson(id).then(() => {
-        setPersons(persons.filter((person) => person.id !== id));
-      });
+      personsService
+        .deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter((person) => person.id !== id));
+          createSuccessMessage(`Deleted ${person.name}`);
+        })
+        .catch(() => {
+          createErrorMessage(
+            `Information of ${person.name} had already been removed from server`
+          );
+          setPersons(persons.filter((person) => person.id !== id));
+        });
     }
   };
 
   return (
     <div>
+      {message && (
+        <p className={`message ${message.type}`}>{message.message}</p>
+      )}
       <h2>Phonebook</h2>
       <div>
         <Search handleSearch={handleSearchInput} />

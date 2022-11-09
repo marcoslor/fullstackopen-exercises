3d2ca836ca81b38d124d7674bffb290493d1710f
Marcos Rios <rioos.marcos@gmail.com>
2022-11-08 22:22:17 -0300

    Exercise 2.11: resolution
    The new requirements will be added on commits on this same branch.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PersonRecord from './Components/PersonRecord'
 import Search from './Components/Search'
 import PhonebookForm from './Components/PhonebookForm'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [searchResults, setSearchResults] = useState()
+
+    useEffect(() => { 
+        axios.get('http://localhost:3001/persons').then(response => {
+            setPersons(response.data)
+        })
+    }, [])
 
     const handleOnSubmit = (event) => {
         event.preventDefault()

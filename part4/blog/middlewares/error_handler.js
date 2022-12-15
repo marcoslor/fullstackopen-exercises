e92b0745aafc13eed9feeacaa6046c1ec4bc1738
Marcos Rios <rioos.marcos@gmail.com>
2022-12-15 00:22:56 -0300

    refactor: make error handler exhaustive

--- part4/blog/middlewares/error_handler.js
+++ part4/blog/middlewares/error_handler.js
@@ -3,21 +3,20 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({
       error: "malformatted id",
     });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({
-      error: error.message,
-    });
   } else if (error.name === "JsonWebTokenError") {
     return response.status(401).json({
       error: "invalid token",
     });
-  } else if (error.name === "APIError") {
+  } else if (error.name === "APIError" || error.name === "ValidationError") {
     return response.status(error.status).json({
       error: error.message,
     });
+  } else {
+    console.log("unknown error");
+    return response.status(500).json({
+      error: "unknown error",
+    });
   }
-  console.log("unknown error", error);
-  next(error);
 };
 
 module.exports = errorHandler;

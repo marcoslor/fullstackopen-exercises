701ed7bd9c4dcbc1296aa7d2bf5a4e8615ca462d
Marcos Rios <rioos.marcos@gmail.com>
2023-01-18 15:16:17 -0300

    Exercises 8.8-8.12: resolution

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,64 @@
+import { useQuery, useMutation } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  if (!props.show || result.loading) {
+    return null;
+  }
+  const authors = result.data.allAuthors;
+
+  const submit = async (event) => { 
+    event.preventDefault();
+    const name = event.target.name.value;
+    const born = Number(event.target.born.value);
+    editAuthor({ variables: { name, born } });
+  };
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="name">name</label>
+          <select name="name" id="name">
+            {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="name">born</label>
+          <input name="born" id="born" required />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  );
+};
+
+export default Authors;
